279d4d6fcf5b5f8ce58a790546eddeb0c31bff01
TonyRoussel <tonydraf@gmail.com>
2014-11-02 02:11:20 +0100

    add command class

---
+++ ogmShellCoreHandler/command.py
@@ -0,0 +1,29 @@
+class cmd(object):
+    def __init__(self, prgString, argList, nxtCmd):
+        self.prg = prgString
+        self.arg = argList
+        self.nxt = nxtCmd
+
+    @property
+    def prg(self):
+        return self._prg
+
+    @prg.setter
+    def prg(self, value):
+        self._prg = value
+
+    @property
+    def arg(self):
+        return self._arg
+
+    @arg.setter
+    def arg(self, value):
+        self._arg = value
+
+    @property
+    def nxt(self):
+        return self._nxt
+
+    @nxt.setter
+    def nxt(self, value):
+        self._nxt = value
